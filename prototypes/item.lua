37d7dcab8dfa8d24904f10456f11ccb3c262b4ad
Gladon4 <alex.mario.grille.amg@gmail.com>
2024-12-07 00:57:10 +0100

    other: Spice name and placeholder graphic

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -4,7 +4,7 @@ data:extend(
             type = "item",
             name = "spice",
             stack_size = 100,
-            icon = "__base__/graphics/icons/iron-ore.png",
+            icon = "__planet-arrakis__/graphics/icons/spice.png",
         }
     }
 )
