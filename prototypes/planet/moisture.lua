0cfecfa4fbcc3ae2a0861567c82060865e5fdbcf
Gladon4 <alex.mario.grille.amg@gmail.com>
2026-02-26 15:20:37 +0100

    feature: Moisture as a new Surface Condition

---
+++ prototypes/planet/moisture.lua
@@ -0,0 +1,13 @@
+data:extend{
+  {
+    type = "surface-property",
+    name = "moisture",
+    default_value = 0
+  }
+}
+
+data.raw["planet"]["nauvis"].surface_properties.moisture = 100
+data.raw["planet"]["gleba"].surface_properties.moisture = 1000
+data.raw["planet"]["fulgora"].surface_properties.moisture = 50
+data.raw["planet"]["vulcanus"].surface_properties.moisture = 1
+data.raw["planet"]["aquilo"].surface_properties.moisture = 10
